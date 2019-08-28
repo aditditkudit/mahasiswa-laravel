cf2500c60092bbb8622ef375270b1fd601bb8502
aditditkudit <adityaadit92@gmail.com>
2019-08-28 22:17:40 +0700

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layout/main')
+
+@section('title', 'Kudit House')
+    
+
+    
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">Hello, world!</h1>        
+            </div>
+        </div>
+    </div>
+@endsection
+    
+
+ 
